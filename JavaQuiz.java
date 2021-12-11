0eeaefdb75744137172169565daef173c6a77eb1
David1587 <95320801+David1587@users.noreply.github.com>
2021-12-11 14:20:08 -0500

    Add files via upload

---
+++ JavaQuiz.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMain.java to edit this template
+ */
+package javaquiz;
+
+/**
+ *
+ * @author 
+ */
+public class JavaQuiz {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //Este es el main class y el inicio del projecyo para este class
+        new Start().setVisible(true);
+    }
+    
+}
